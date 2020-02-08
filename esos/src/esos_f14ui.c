80b6d165fe8cab48c0c151c89fe936281e69b3d5
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-02-08 11:33:24 -0600

    Re-added esos_f14ui.c

---
+++ esos/src/esos_f14ui.c
@@ -0,0 +1,258 @@
+/*******************************************************************
+ * 
+ * C code framework for ESOS user-interface (UI) service
+ * 
+ *    requires the EMBEDDED SYSTEMS target rev. F14
+ * 
+ * ****************************************************************/
+# include <esos_f14ui.h>
+# include <revF14.h>
+
+// PRIVATE FUNCTIONS
+inline void _esos_uiF14_setRPGCounter (uint16_t newValue) {
+    _st_esos_uiF14Data.u16_RPGCounter = newValue;
+    return;
+}
+
+inline void _esos_uiF14_setLastRPGCounter (uint16_t newValue) {
+    _st_esos_uiF14Data.u16_lastRPGCounter = newValue;
+    return;
+}
+
+// PUBLIC SWITCH FUNCTIONS
+inline BOOL esos_uiF14_isSW1Pressed (void) {
+    return (_st_esos_uiF14Data.b_SW1Pressed==TRUE);
+}
+
+inline BOOL esos_uiF14_isSW1Released (void) {
+    return (_st_esos_uiF14Data.b_SW1Pressed==FALSE);
+}
+
+inline BOOL esos_uiF14_isSW1DoublePressed (void) {
+    return (_st_esos_uiF14Data.b_SW1DoublePressed==TRUE);
+}
+
+inline BOOL esos_uiF14_isSW2Pressed (void) {
+    return (_st_esos_uiF14Data.b_SW2Pressed==TRUE);
+}
+
+inline BOOL esos_uiF14_isSW2Released (void) {
+    return (_st_esos_uiF14Data.b_SW2Pressed==FALSE);
+}
+
+inline BOOL esos_uiF14_isSW2DoublePressed (void) {
+    return (_st_esos_uiF14Data.b_SW2DoublePressed==TRUE);
+}
+
+inline BOOL esos_uiF14_isSW3Pressed (void) {
+    return (_st_esos_uiF14Data.b_SW3Pressed==TRUE);
+}
+
+inline BOOL esos_uiF14_isSW3Released (void) {
+    return (_st_esos_uiF14Data.b_SW3Pressed==FALSE);
+}
+
+inline BOOL esos_uiF14_isSW3DoublePressed (void) {
+    return (_st_esos_uiF14Data.b_SW3DoublePressed==TRUE);
+}
+
+
+// PUBLIC LED FUNCTIONS
+
+//Ryan: I think some of these functions are intended to actually
+//trigger LED changes, so I'm updating the LED register
+
+inline BOOL esos_uiF14_isLED1On (void) {
+    return (_st_esos_uiF14Data.b_LED1On==TRUE);
+}
+
+inline BOOL esos_uiF14_isLED1Off (void) {
+    return (_st_esos_uiF14Data.b_LED1On==FALSE);
+}
+
+inline void esos_uiF14_turnLED1On (void) {
+    LED1 = 1;
+    _st_esos_uiF14Data.b_LED1On = TRUE;
+    return;
+}
+
+inline void esos_uiF14_turnLED1Off (void) {
+    LED1 = 0;
+    _st_esos_uiF14Data.b_LED1On = FALSE;
+    return;
+}
+
+inline void esos_uiF14_toggleLED1 (void) {
+    LED1 = !LED1;
+    _st_esos_uiF14Data.b_LED1On ^= 1;
+    return;
+}
+
+inline void esos_uiF14_flashLED1( uint16_t u16_period1) {
+    _st_esos_uiF14Data.u16_LED1FlashPeriod = u16_period1;
+    return
+}
+
+inline BOOL esos_uiF14_isLED2On (void) {
+    return (_st_esos_uiF14Data.b_LED2On==TRUE);
+}
+
+inline BOOL esos_uiF14_isLED2Off (void) {
+    return (_st_esos_uiF14Data.b_LED2On==FALSE);
+}
+
+inline void esos_uiF14_turnLED2On (void) {
+    LED2 = 1;
+    _st_esos_uiF14Data.b_LED2On = TRUE;
+    return;
+}
+
+inline void esos_uiF14_turnLED2Off (void) {
+    LED2 = 0;
+    _st_esos_uiF14Data.b_LED2On = FALSE;
+    return;
+}
+
+inline void esos_uiF14_toggleLED2 (void) {
+    LED2 = !LED2;
+    _st_esos_uiF14Data.b_LED2On ^= 1;
+    return;
+}
+
+inline void esos_uiF14_flashLED2( uint16_t u16_period2) {
+    _st_esos_uiF14Data.u16_LED2FlashPeriod = u16_period2;
+    return
+}
+
+inline BOOL esos_uiF14_isLED3On (void) {
+    return (_st_esos_uiF14Data.b_LED3On==TRUE);
+}
+
+inline BOOL esos_uiF14_isLED3Off (void) {
+    return (_st_esos_uiF14Data.b_LED3On==FALSE);
+}
+
+inline void esos_uiF14_turnLED3On (void) {
+    //LED3 is low active
+    LED3 = 0;
+    _st_esos_uiF14Data.b_LED3On = TRUE;
+    return;
+}
+
+inline void esos_uiF14_turnLED3Off (void) {
+    LED3 = 1;
+    _st_esos_uiF14Data.b_LED3On = FALSE;
+    return;
+}
+
+inline void esos_uiF14_toggleLED3 (void) {
+    LED3 = !LED3;
+    _st_esos_uiF14Data.b_LED3On ^= 1;
+    return;
+}
+
+inline void esos_uiF14_flashLED3( uint16_t u16_period3) {
+    _st_esos_uiF14Data.u16_LED3FlashPeriod = u16_period3;
+    return
+}
+
+/****** RED, GREEN, and YELLOW functions *******/
+
+inline void esos_uiF14_turnRedLEDOn (void) {
+    //Red LED is LED1
+    esos_uiF14_turnLED1On();
+}
+
+inline void esos_uiF14_turnRedLEDOff (void) {
+    esos_uiF14_turnLED1Off();
+}
+
+inline void esos_uiF14_turnGreenLEDOn (void) {
+    //Green LED is LED3
+    esos_uiF14_turnLED3On();
+}
+
+inline void esos_uiF14_turnGreenLEDOff (void) {
+    esos_uiF14_turnLED3Off();
+}
+
+inline void esos_uiF14_turnYellowLEDOn (void) {
+    //Yellow LED is LED2
+    esos_uiF14_turnLED2On();
+}
+
+inline void esos_uiF14_turnYellowLEDOff (void) {
+    esos_uiF14_turnLED2Off();
+}
+
+// PUBLIC RPG FUNCTIONS
+
+inline uint16_t esos_uiF14_getRpgValue_u16 ( void ) {
+    return _st_esos_uiF14Data.u16_RPGCounter;
+}
+
+inline BOOL esos_uiF14_isRpgTurning ( void ) {
+    return (esos_uiF14_getRpgVelocity_i16() != 0);
+}
+
+inline BOOL esos_uiF14_isRpgTurningSlow( void ) {
+  // not yet implemented
+}
+
+inline BOOL esos_uiF14_isRpgTurningMedium( void ) {
+  // not yet implemented
+}
+
+inline BOOL esos_uiF14_isRpgTurningFast( void ) {
+  // not yet implemented
+}
+
+inline BOOL esos_uiF14_isRpgTurningCW( void ) {
+  // not yet implemented
+}
+
+inline BOOL esos_uiF14_isRpgTurningCCW( void ) {
+  // not yet implemented
+}
+
+int16_t esos_uiF14_getRpgVelocity_i16( void ) {
+  // not yet implemented
+}
+
+// UIF14 INITIALIZATION FUNCTION
+
+void config_esos_uiF14() {
+  // setup your UI implementation
+
+  //Step 1: setup LEDs
+  //
+  CONFIG_LED1();
+  CONFIG_LED2();
+  CONFIG_LED3();
+
+  //Step 2: setup switches
+  CONFIG_SW1();
+  CONFIG_SW2();
+  CONFIG_SW3();
+
+  //Step 3: setup RPG
+  CONFIG_RPGA();
+  CONFIG_RPGB();
+
+  //TODO
+
+
+  esos_RegisterTask( __uiF14_task );
+}
+
+
+// UIF14 task to manage user-interface
+ESOS_USER_TASK( __esos_uiF14_task ){
+  
+  ESOS_TASK_BEGIN();
+  while(TRUE) {
+    // do your UI stuff here
+    ESOS_TASK_WAIT_TICKS( __ESOS_UIF14_UI_PERIOD );
+  }
+  ESOS_TASK_END();
+}
