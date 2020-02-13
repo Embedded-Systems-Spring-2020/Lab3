528c28bac55f5c949e3566073fcc84bbfaae10bf
Andrew Yingst <andrewlyingst@gmail.com>
2020-02-13 08:03:57 -0600

    replaced outString command with ESOS_TASK_WAIT_ON_SEND_STRING and commented out dataXfer stuff

--- BUILD_DIR/main.c
+++ BUILD_DIR/main.c
@@ -11,7 +11,7 @@
 #include "pic24_util.h"
 #include "pic24_serial.h"
 #include "revF14.h"
-#include <dataXfer.h> //this is the library that uses the uc/pc variable on the bootloader
+#include "esos_comm.h"
 
  
 ESOS_USER_TASK(demoLEDsAndSwitches) {
@@ -65,61 +65,61 @@ ESOS_USER_TASK(demoLEDsAndSwitches) {
 	ESOS_TASK_END();
 }	
 		
-enum { u16_DOUBLE_PRESS_UPPER_MS_NDX, // the variables that will be displayed on uc/pc variable tab
+/*enum { u16_DOUBLE_PRESS_UPPER_MS_NDX, // the variables that will be displayed on uc/pc variable tab
 		u16_RPG_SLOW_MS_NDX,
 		u16_RPG_MEDIUM_MS_NDX, 
 		u16_RPG_FAST_MS_NDX,
 		u16_RPG_PERIOD_NDX,
-		U16_RPG_VALUE_NDX};
+		U16_RPG_VALUE_NDX};*/
 
 
 ESOS_USER_TASK(drawDisplay) {
     ESOS_TASK_BEGIN();
-		initDataXfer(); 
+		/*initDataXfer(); 
 		//format for below... var name from enum above, actual var name, T/F changeable, label
 		SPECIFY_VAR(u16_DOUBLE_PRESS_UPPER_MS_NDX, _st_esos_uiF14Data.u16_doublePressUpperMs, TRUE, "%u", "Upper double press time limit");
 		SPECIFY_VAR(u16_RPG_SLOW_MS_NDX, _st_esos_uiF14Data.u16_RPGNotMovingToSlowPeriodMs, TRUE, "%u", "Time in ms between RPG clicks -not moving to slow");
 		SPECIFY_VAR(u16_RPG_MEDIUM_MS_NDX, _st_esos_uiF14Data.u16_RPGSlowToMediumPeriodMs, TRUE, "%u", "Time in ms between RPG clicks -slow to medium");
 		SPECIFY_VAR(u16_RPG_FAST_MS_NDX, _st_esos_uiF14Data.u16_RPGMediumToFastPeriodMs, TRUE, "%u", "Time in ms between RPG clicks -medium to fast");
 		SPECIFY_VAR(u16_RPG_PERIOD_NDX, _st_esos_uiF14Data.u16_RPGPeriodMs, FALSE, "%u", "Time between clicks in ms");
-		SPECIFY_VAR(U16_RPG_VALUE_NDX, _st_esos_uiF14Data.i16_RPGCounter, FALSE, "%u", "Current RPG Counter");	
-/*		while(1){
-			if (esos_uiF14_isSW1Pressed()){outString("\nSw1 is Pushed  ");}
-			if (esos_uiF14_isSW1Released()){outString("Sw1 is Released");}
-			if (esos_uiF14_isSW1DoublePressed()){outString("Sw1 is Double Pressed\n");
+		SPECIFY_VAR(U16_RPG_VALUE_NDX, _st_esos_uiF14Data.i16_RPGCounter, FALSE, "%u", "Current RPG Counter");	*/
+		while(1){
+			if (esos_uiF14_isSW1Pressed()){ESOS_TASK_WAIT_ON_SEND_STRING("\nSw1 is Pushed  ");}
+			if (esos_uiF14_isSW1Released()){ESOS_TASK_WAIT_ON_SEND_STRING("Sw1 is Released");}
+			if (esos_uiF14_isSW1DoublePressed()){ESOS_TASK_WAIT_ON_SEND_STRING("Sw1 is Double Pressed\n");
 				ESOS_TASK_WAIT_TICKS(300);
-			} else {outString("Sw1 is NOT Double Pressed\n");}
-			if (esos_uiF14_isSW2Pressed()){outString("Sw2 is Pushed  ");}
-			if (esos_uiF14_isSW2Released()){outString("Sw2 is Released");}
-			if (esos_uiF14_isSW2DoublePressed()){outString("Sw2 is Double Pressed\n");
+			} else {ESOS_TASK_WAIT_ON_SEND_STRING("Sw1 is NOT Double Pressed\n");}
+			if (esos_uiF14_isSW2Pressed()){ESOS_TASK_WAIT_ON_SEND_STRING("Sw2 is Pushed  ");}
+			if (esos_uiF14_isSW2Released()){ESOS_TASK_WAIT_ON_SEND_STRING("Sw2 is Released");}
+			if (esos_uiF14_isSW2DoublePressed()){ESOS_TASK_WAIT_ON_SEND_STRING("Sw2 is Double Pressed\n");
 				ESOS_TASK_WAIT_TICKS(300);
-			} else {outString("Sw2 is NOT Double Pressed\n");}
-			if (esos_uiF14_isSW3Pressed()){outString("Sw3 is Pushed  ");}
-			if (esos_uiF14_isSW3Released()){outString("Sw3 is Released");}
-			if (esos_uiF14_isSW3DoublePressed()){outString("Sw3 is Double Pressed\n\n");
+			} else {ESOS_TASK_WAIT_ON_SEND_STRING("Sw2 is NOT Double Pressed\n");}
+			if (esos_uiF14_isSW3Pressed()){ESOS_TASK_WAIT_ON_SEND_STRING("Sw3 is Pushed  ");}
+			if (esos_uiF14_isSW3Released()){ESOS_TASK_WAIT_ON_SEND_STRING("Sw3 is Released");}
+			if (esos_uiF14_isSW3DoublePressed()){ESOS_TASK_WAIT_ON_SEND_STRING("Sw3 is Double Pressed\n\n");
 				ESOS_TASK_WAIT_TICKS(300);
-			} else {outString("Sw3 is NOT Double Pressed\n\n");}
+			} else {ESOS_TASK_WAIT_ON_SEND_STRING("Sw3 is NOT Double Pressed\n\n");}
 
 			if (esos_uiF14_isRPGTurning()) {
-				outString("RPG is Turning ");
+				ESOS_TASK_WAIT_ON_SEND_STRING("RPG is Turning ");
 				if (esos_uiF14_isRPGTurningCW()) { 
-					outString("CW ");
+					ESOS_TASK_WAIT_ON_SEND_STRING("CW ");
 				} else {
-					outString("CCW");
+					ESOS_TASK_WAIT_ON_SEND_STRING("CCW");
 				}
 				if (esos_uiF14_isRPGTurningSlow()) {
-					outString(" Slowly\n");
+					ESOS_TASK_WAIT_ON_SEND_STRING(" Slowly\n");
 				} else if (esos_uiF14_isRPGTurningMedium()) {
-					outString(" Mediumly\n");
+					ESOS_TASK_WAIT_ON_SEND_STRING(" Mediumly\n");
 				} else { 
-					outString(" Fastly\n");
+					ESOS_TASK_WAIT_ON_SEND_STRING(" Fastly\n");
 				}
 			} else {
-				outString("RPG is Not Turning");
+				ESOS_TASK_WAIT_ON_SEND_STRING("RPG is Not Turning");
 			}
 			ESOS_TASK_WAIT_TICKS(50);
-			outString("\n\n\n\n\n\n\n\n"); //these newlines clear the screen and allow overwrite
-		}*/
+			ESOS_TASK_WAIT_ON_SEND_STRING("\n\n\n\n\n\n\n\n"); //these newlines clear the screen and allow overwrite
+		}
 	ESOS_TASK_END();
 }
 void user_init(){
